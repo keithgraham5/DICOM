aa151eba1d684cbf3b1358f6f19b30b97798f2d3
keithgraham5 <keith.graham5@nhs.net>
2021-04-03 13:57:25 +0100

    to run on laptop

--- DICOM_test.py
+++ DICOM_test.py
@@ -15,14 +15,20 @@ import numpy as np
 import pydicom as dicom
 import matplotlib.pyplot as plt
 
+
 #file paths
 src = '/Users/keithgraham/PycharmProjects/ICT/RD'
 dst = '/Users/keithgraham/PycharmProject/ICT/DICOM_complete'
 
-#namespaces
+class Dicom:
+    '''open, read, calibrate and normalise Dicom archive'''
+
+    def __init__(self, src):
+        '''Initialize src attribute'''
+        self.src = src
 
 #loop through directory identiying files ending with .dcm
-def loopDicomFiles():
+    def loopDicomFiles(self):
 '''
 The iterable is an object with elements that can be looped over.
     Directory containing dicom files
@@ -35,40 +41,42 @@ Lexixal scoping varible are determined entirely by the location in the souce cod
     not clash with loops in other functions
     local namespaces cannot be seen or reffered to outside the function
 '''
-def loopDicomFile(src):
-openedFiles = [] # empty list
-for i in glob.iglob('/Users/keithgraham/PycharmProjects/ICT/RD/**/*.dcm', recursive=True):
-    if i.endswith('.dcm'): # if file in loop ends with .dcm
-        openedFiles.append(i) #append said file ending in dcm to emtpy list
+# def loopDicomFile(self):
+# openedFiles = [] # empty list
+# for i in glob.iglob('/Users/keithgraham/PycharmProjects/ICT/RD/**/*.dcm', recursive=True):
+#     if i.endswith('.dcm'): # if file in loop ends with .dcm
+#         openedFiles.append(i) #append said file ending in dcm to emtpy list
 
-# def readDicomFile():
-# readFiles = []
-# for i in openedFiles:
-#     dataset = dicom.dcmread(images)
-#     readFiles.append(dataset)
-#
-# def calibrateDicomArrays():
-# calibratedFiles = []
-# for i in readFiles:
-#     calibrateArray = arry.pixel_array * arry.DoseGridScaling
-#     calibratedFiles.append(calibrateArray)
-#
-#
-#
-# def calibrateArrays():
-# z, x, y = calibratedFiles[0].shape
-# combineArray = np.zeros([z, x, y])
-#
-# for beam in calibratedFiles:
-#     combineArray = combineArray + beam
+    openedFiles = []
+    for i in os.listdir(self.src):
+        if i.endswith(".dcm"):
+            openedFiles.append(i)
+    print(openedFiles)
 
+    # def readDicomFile(self):
+    # readFiles = []
+    # for i in openedFiles:
+    #     dataset = dicom.dcmread(images)
+    #     readFiles.append(dataset)
+    #
+    # def calibrateDicomArrays(self):
+    # calibratedFiles = []
+    # for i in readFiles:
+    #     calibrateArray = arry.pixel_array * arry.DoseGridScaling
+    #     calibratedFiles.append(calibrateArray)
+    #
+    #
+    #
+    # def calibrateArrays(self):
+    # z, x, y = calibratedFiles[0].shape
+    # combineArray = np.zeros([z, x, y])
+    #
+    # for beam in calibratedFiles:
+    #     combineArray = combineArray + beam
 
+Dicom()
 #Main program
-loopDicomFiles()
-# readDicomFile()
-# calibrateDicomImages()
-# calibrateArray()
-#
+
 #
 # plt.imshow(combineArray[30,:,:])
 # plt.show()
