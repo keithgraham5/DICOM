f3f12f6cedd66d7bf1434ba72231344cc0b3effa
keithgraham5 <keith.graham5@nhs.net>
2021-04-21 19:47:37 +0100

    updating across systems

--- DICOM_prototype_V.2.py
+++ DICOM_prototype_V.2.py
@@ -13,38 +13,51 @@ import matplotlib.pyplot as plt
 #file paths
 src = '/Users/keithgraham/PycharmProjects/ICT/RD'
 dst = '/Users/keithgraham/PycharmProject/ICT/DICOM_complete'
-
-# loop through directory identiying files ending with .dcm
-dicom_files = [] #creates and empty list
-for file_name in glob.iglob('/Users/keithgraham/PycharmProjects/ICT/RD/**/*.dcm', recursive=True):
-    if file_name.endswith('.dcm'):
-        dicom_files.append(file_name)
-
-
-slices = []
-for images in dicom_files:
-    ds = dicom.dcmread(images)
-    slices.append(ds)
+def list_dcmfiles_from_directory(src):
+    '''loops through directory to find file ext ending in .dcm appends these file to list '''
+    dicom_files = [] #creates and empty list
+    for file_name in glob.iglob('/Users/keithgraham/PycharmProjects/ICT/RD/**/*.dcm', recursive=True):
+        if file_name.endswith('.dcm'):
+            dicom_files.append(file_name)
+    return (dicom_files)
 
 
-
-calibrateArrays = []
-for arry in slices:
-    calibrateArray = arry.pixel_array * arry.DoseGridScaling
-    calibrateArrays.append(calibrateArray)
-
-
-
-
-# only showing one image
-z, x, y = calibrateArrays[0].shape
-combineArray = np.zeros([z, x, y])
-
-for beam in calibrateArrays:
-    combineArray = combineArray + beam
-
-
-# Creates an empty array based on the shape of the first array
-
-plt.imshow(combineArray[30,:,:])
-plt.show()
+# def read_dicom_files(dicom_files):
+#     '''using pydicom.dcmread to make dicom files readable,
+#     each of the dicom represent a single radiation beam devilvered over a complete treatment plan '''
+#     single_radiation_beam = []
+#     for images in dicom_files:
+#         ds = dicom.dcmread(images)
+#         single_radiation_beam.append(ds)
+#     return (single_radiation_beam)
+#
+#
+#
+# def calibrate_arrays(single_radiation_beam):
+#         '''Each array require calibration against their individual Dose Grid scaling '''
+#         calibrateArrays = []
+#         for arry in slices:
+#             calibrateArray = arry.pixel_array * arry.DoseGridScaling
+#             calibrateArrays.append(calibrateArray)
+#         return (calibrateArrays)
+#
+#
+# def combine_array(calibrateArray):
+#     '''create an empty array in which to sum the totals of all arrays'''
+#     z, x, y = calibrateArrays[0].shape
+#     combineArray = np.zeros([z, x, y])
+#     for beam in calibrateArrays:
+#         combineArray = combineArray + beam
+#     return (combineArray)
+#
+#
+#
+#
+# # plt.imshow(combineArray[30,:,:])
+# # plt.show()
+#
+x = list_dcmfiles_from_directory(src)
+print(x)
+# read_dicom_files(dicom_files)
+# calibrate_arrays(single_radiation_beam)
+# combine_array(calibrateArray)
